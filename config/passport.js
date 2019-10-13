b381ac0707158c8dceee756ae689344c016fe9be
Muhammed ogunsanya <mspeaks910@gmail.com>
2019-10-13 13:58:22 +0100

    made changes

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
 
-const mongoose = require ('mongoose');
 
 const bcrypt = require ('bcryptjs');
 
@@ -44,4 +43,4 @@ module.exports = (passport) =>{
 
     }
 
-    //end
+    //changes
